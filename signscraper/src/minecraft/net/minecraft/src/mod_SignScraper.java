370972cf5f7dfa41cdd0b0dfeff887b491f010b3
wav'painter <wavpainter@gmail.com>
2025-02-14 22:47:05 +1100

    Created mod java file

---
+++ signscraper/src/minecraft/net/minecraft/src/mod_SignScraper.java
@@ -0,0 +1,13 @@
+package net.minecraft.src;
+
+public class mod_SignScraper extends BaseMod {
+    @Override
+    public String Version() {
+        return "SignScraper v1.0";
+    }
+
+    @Override
+    public void ModsLoaded() {
+
+    }
+}
