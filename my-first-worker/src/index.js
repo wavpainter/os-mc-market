0b92eed681ea89c080cc38b54328622000c4cd23
wav'painter <wavpainter@gmail.com>
2025-02-24 19:11:56 +1100

    Cache example

---
+++ my-first-worker/src/index.js
@@ -0,0 +1,36 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+let clearCache = false;
+
+export default {
+	async fetch(request, env, ctx) {
+		const cache = caches.default;
+
+		const cacheKey = new Request(request.url,request);
+
+		if(clearCache) {
+			await cache.delete(cacheKey);
+			return new Response("Cleared cache");
+		}else {
+			let response = await cache.match(cacheKey,request);
+
+			if (!response) {
+				response = new Response(Math.random());
+	
+				response.headers.set('Cache-Control','s-maxage=5');
+	
+				await cache.put(cacheKey,response.clone());
+			}
+			return response;
+		}
+
+	},
+};
